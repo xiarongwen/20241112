5c583a608887693d0eb0ac0191abf52eb2e0f56a
xiarongwen <1196452041@qq.com>
2024-10-11 01:31:22 +0800

    feat: 完成上传pdf以及旋转 剩余旋转动画以及响应式布局

--- app/component/pdf-processor/index.tsx
+++ app/component/pdf-processor/index.tsx
@@ -6,106 +6,130 @@ import './index.scss'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
-const PDFProcessor = () => {
-  const [file, setFile] = useState(null as any);
-  const [numPages, setNumPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [rotationAngle, setRotationAngle] = useState(0);
+const PDFProcessor: React.FC<{}> = () => {
+    const [file, setFile] = useState(null as any);
+    const [numPages, setNumPages] = useState(0);
+    const [rotationAngle, setRotationAngle] = useState(0);
+    const [zoomLevel, setZoomLevel] = useState(0.4);
+    const [pageRotations, setPageRotations] = useState<number[]>([]);
 
-  const onFileChange = (event: any) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
-  };
+    const onFileChange = (event: any) => {
+        const selectedFile = event.target.files[0];
+        setFile(selectedFile);
+    };
 
-  const onDocumentLoadSuccess = ({ numPages: totalPages }) => {
-    setNumPages(totalPages);
-  };
+    const onDocumentLoadSuccess = ({ numPages: totalPages }) => {
+        setNumPages(totalPages);
+    };
 
-  const goToPreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        const droppedFile = event.dataTransfer.files[0];
+        setFile(droppedFile);
+    };
+
+    const handleClickUpload = () => {
+        const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+        fileInput.click();
+    };
+
+    const onRemove = () => {
+        setFile(null)
     }
-  };
 
-  const goToNextPage = () => {
-    if (currentPage < numPages) {
-      setCurrentPage(currentPage + 1);
+    const handleZoomIn = () => {
+        if (zoomLevel < 1) {
+            setZoomLevel(prevZoom => prevZoom + 0.1);
+        }
+
+    };
+
+    const handleZoomOut = () => {
+        if (zoomLevel > 0.22) {
+            setZoomLevel(prevZoom => prevZoom - 0.1 > 0 ? prevZoom - 0.1 : prevZoom);
+        }
+
+    };
+
+    const handlePageClick = (pageNumber: number) => {
+        const newRotations = [...pageRotations];
+        const currentRotation = newRotations[pageNumber - 1] || 0;
+        newRotations[pageNumber - 1] = currentRotation + 90;
+        setPageRotations(newRotations);
+    };
+
+    const onRotateAll = () => {
+        setRotationAngle((prevAngle) => prevAngle + 90);
+        const updatedPageRotations = pageRotations.map(
+            (pageRotation, index) => pageRotation + rotationAngle
+        );
+        setPageRotations(updatedPageRotations);
+    };
+
+
+    const UploadWarp = () => {
+        return (
+            <>
+                <div style={{
+                    width: '275px',
+                    height: '350px',
+                    border: '2px dashed #ccc',
+                    padding: '20px',
+                    textAlign: 'center',
+                    minHeight: '200px',
+                    cursor: 'pointer',
+                    margin:'0 auto',
+                    background:"#fff"
+                }}
+                    onClick={handleClickUpload}
+                    onDragOver={handleDragOver}
+                    onDrop={handleDrop}>
+                    <input id='fileInput' type="file" onChange={onFileChange} style={{ display: 'none' }} />
+                    <p>Click to upload or drag and drop</p>
+                </div>
+            </>
+        )
     }
-  };
 
-  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-  };
+    const ViewWarp = () => {
+        return (
+            <>
+                <div className='top-button'>
+                    <Button onClick={onRotateAll} label='Rotate all' />
+                    <Button onClick={onRemove} type='remove' label='Remove PDF' />
+                    <Button onClick={handleZoomIn} type='circle' label='+' />
+                    <Button onClick={handleZoomOut} type='circle' label='-' />
+                </div>
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    const droppedFile = event.dataTransfer.files[0];
-    setFile(droppedFile);
-  };
+                <div className='document-wrap'>
+                    <Document onItemClick={({ pageNumber }) => alert('Clicked an item from page ' + pageNumber + '!')} rotate={rotationAngle} className='view-warp' file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                        {Array.from({ length: numPages }, (_, index) => (
+                            <div className='page-warp'>
+                                <Page  className={'pagestyle'} scale={zoomLevel} onClick={() => handlePageClick(index + 1)}
+                                    rotate={
+                                        pageRotations[index] || rotationAngle
+                                    }
+                                    renderAnnotationLayer={false} renderTextLayer={false} pageNumber={index + 1} />
+                                <p>{index + 1}</p>
+                            </div>
+                        ))}
 
-  const handleClickUpload = () => {
-    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-    fileInput.click();
-  };
+                    </Document>
+                </div>
+            </>
+        )
+    }
 
-  const onRemove = () => {
-    setFile(null)
-  }
-
-  const onRotateAll = async () => {
-   setRotationAngle((prevAngle) => prevAngle + 90);
-  };
-
-  const UploadWarp = () => {
     return (
         <>
-    <div style={{
-        width:'275px',
-        height:'350px',
-        border: '2px dashed #ccc',
-        padding: '20px',
-        textAlign: 'center',
-        minHeight: '200px',
-        cursor: 'pointer',
-      }}
-        onClick={handleClickUpload}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}>
-      <input id='fileInput' type="file" onChange={onFileChange} style={{ display: 'none' }}  />
-      <p>Click to upload or drag and drop</p>
-    </div>
+            {!!file ? ViewWarp() : UploadWarp()}
         </>
-    )
-  }
-
-  const ViewWarp = () => {
-    return (
-        <>
-        <div>
-            <Button onClick={onRotateAll} label='Rotate all' />
-            <Button onClick={onRemove} type='remove' label='Remove PDF' />
-        </div>
-        
-        <div className='document-wrap'>
-          <Document  rotate={rotationAngle} className='view-warp' file={file} onLoadSuccess={onDocumentLoadSuccess}>
-          {Array.from({ length: numPages }, (_, index) => (
-            <div className='page-warp'>
-                <Page renderAnnotationLayer={false} renderTextLayer={false} pageNumber={index + 1} />
-                <p>{index + 1}</p>
-            </div>
-            ))}
-            
-          </Document>
-        </div>
-        </>
-    )
-  }
-
-  return (
-       <>
-        {!!file ? ViewWarp() : UploadWarp()}
-       </>
-  );
+    );
 };
 
 export default PDFProcessor;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
 'use client'
 import PDFProcessor from './component/pdf-processor'
+import 'tailwindcss/tailwind.css';
+import './app.scss'
+import './page.scss'
 
-const  Home = () => {
-  return(
-    <div>
-      <PDFProcessor />
+const Home = () => {
+  return (
+    <div className="flex flex-col min-h-screen" style={{ background: "#f7f5ee" }}>
+      <header className="bg-white bg-gray-100 p-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Rotate PDF Pages</h1>
+        <div className="links">
+          <span>Pricing</span>
+          <span>Chrome extension</span>
+          <span>Use cases</span>
+          <span>Get started→</span>
+        </div>
+      </header>
+      <div className='container'>
+        <h1 className='text-5xl font-serif'>
+          Rotate PDF Pages
+        </h1>
+        <p className='container-p mt-2 text-gray-600 max-w-lg mx-auto'>
+          Simply click on a page to rotate it. You can then download your modified PDF.
+        </p>
+      </div>
+      <section>
+        <PDFProcessor />
+      </section>
+      <footer>
+
+      </footer>
     </div>
   )
 }
