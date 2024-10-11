300734156fb42e16659db512f5daff88d0e7c9c5
xiarongwen <1196452041@qq.com>
2024-10-11 14:42:58 +0800

    feat:样式修改

--- app/component/pdf-processor/index.tsx
+++ app/component/pdf-processor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import Button from '../button';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -10,7 +10,8 @@ const PDFProcessor: React.FC<{}> = () => {
     const [file, setFile] = useState(null as any);
     const [numPages, setNumPages] = useState(0);
     const [rotationAngle, setRotationAngle] = useState(0);
-    const [zoomLevel, setZoomLevel] = useState(0.4);
+    const [zoomLevel, setZoomLevel] = useState(1);
+    const [wrapLevel, setWrapLevel] = useState(200);
     const [pageRotations, setPageRotations] = useState<number[]>([]);
 
     const onFileChange = (event: any) => {
@@ -42,9 +43,16 @@ const PDFProcessor: React.FC<{}> = () => {
         setFile(null)
     }
 
+    const onDownload = () => {
+       
+      };
+    
+
     const handleZoomIn = () => {
-        if (zoomLevel < 1) {
+        if (zoomLevel < 1.6) {
             setZoomLevel(prevZoom => prevZoom + 0.1);
+            setWrapLevel(prevWrap => prevWrap + 50);
+
         }
 
     };
@@ -52,6 +60,7 @@ const PDFProcessor: React.FC<{}> = () => {
     const handleZoomOut = () => {
         if (zoomLevel > 0.22) {
             setZoomLevel(prevZoom => prevZoom - 0.1 > 0 ? prevZoom - 0.1 : prevZoom);
+            setWrapLevel(prevWrap => prevWrap - 50);
         }
 
     };
@@ -83,43 +92,65 @@ const PDFProcessor: React.FC<{}> = () => {
                     textAlign: 'center',
                     minHeight: '200px',
                     cursor: 'pointer',
-                    margin:'0 auto',
-                    background:"#fff"
+                    margin: '0 auto',
+                    background: "#fff"
                 }}
                     onClick={handleClickUpload}
                     onDragOver={handleDragOver}
                     onDrop={handleDrop}>
-                    <input id='fileInput' type="file" onChange={onFileChange} style={{ display: 'none' }} />
+                    <input id='fileInput' accept='.pdf' type="file" onChange={onFileChange} style={{ display: 'none' }} />
                     <p>Click to upload or drag and drop</p>
                 </div>
             </>
         )
     }
-
+    // 
     const ViewWarp = () => {
         return (
             <>
-                <div className='top-button'>
+                <div className='flex justify-center items-center space-x-3 selecto-ignore'>
                     <Button onClick={onRotateAll} label='Rotate all' />
                     <Button onClick={onRemove} type='remove' label='Remove PDF' />
                     <Button onClick={handleZoomIn} type='circle' label='+' />
                     <Button onClick={handleZoomOut} type='circle' label='-' />
                 </div>
+                <div className='flex flex-wrap justify-center'>
+                    <Document rotate={rotationAngle} file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                        <div className='flex flex-wrap justify-center'>
+                            {Array.from({ length: numPages }, (_, index) => (
+                                <div className='m-3' style={{maxWidth: `${wrapLevel}px`, flex:` 0 0 ${wrapLevel}px`}} onClick={() => handlePageClick(index + 1)}>
+                                    <div className='relative cursor-pointer'>
+                                        <div className='overflow-hidden transition-transform'>
+                                            <div className='relative h-full w-full flex flex-col justify-between items-center shadow-md p-3 bg-white hover:bg-gray-50'>
+                                            <div className='pointer-events-none w-full shrink'>
+                                                <div>
+                                                <Page 
+                                                    renderMode="canvas"
+                                                    width={180}
+                                                    scale={zoomLevel}
+                                                    rotate={
+                                                        pageRotations[index] || rotationAngle
+                                                    }
+                                                    renderTextLayer={false} pageNumber={index + 1} />
+                                                </div>
+                                                
+                                            </div>
 
-                <div className='document-wrap'>
-                    <Document onItemClick={({ pageNumber }) => alert('Clicked an item from page ' + pageNumber + '!')} rotate={rotationAngle} className='view-warp' file={file} onLoadSuccess={onDocumentLoadSuccess}>
-                        {Array.from({ length: numPages }, (_, index) => (
-                            <div className='page-warp'>
-                                <Page  className={'pagestyle'} scale={zoomLevel} onClick={() => handlePageClick(index + 1)}
-                                    rotate={
-                                        pageRotations[index] || rotationAngle
-                                    }
-                                    renderAnnotationLayer={false} renderTextLayer={false} pageNumber={index + 1} />
-                                <p>{index + 1}</p>
-                            </div>
-                        ))}
+                                            <div className='w-[90%] text-center shrink-0 text-xs italic overflow-hidden text-ellipsis whitespace-nowrap'>{index + 1}</div>
+                                            </div>
+                                            
+                                        </div>
 
+                                    </div>
+
+                                </div>
+                            ))}
+                        </div>
                     </Document>
+                </div>
+                <div>
+                <Button onClick={onDownload} label='download' />
+
                 </div>
             </>
         )
--- app/page.tsx
+++ app/page.tsx
@@ -16,19 +16,33 @@ const Home = () => {
           <span>Get started→</span>
         </div>
       </header>
-      <div className='container'>
-        <h1 className='text-5xl font-serif'>
-          Rotate PDF Pages
-        </h1>
-        <p className='container-p mt-2 text-gray-600 max-w-lg mx-auto'>
-          Simply click on a page to rotate it. You can then download your modified PDF.
-        </p>
-      </div>
-      <section>
+
+      <div className='container mx-auto py-20 space-y-5'>
+        <div className='flex flex-col text-center !mb-10 space-y-5'>
+          <h1 className='text-5xl font-serif'>
+            Rotate PDF Pages
+          </h1>
+          <p className='container-p mt-2 text-gray-600 max-w-lg mx-auto'>
+            Simply click on a page to rotate it. You can then download your modified PDF.
+          </p>
+        </div>
+        <section >
         <PDFProcessor />
       </section>
-      <footer>
-
+      </div>
+      
+      <footer className="bg-white" aria-labelledby="footer-heading">
+      <div className='mx-auto max-w-7xl px-6 pb-8 mt-8 sm:mt-12 lg:px-8 lg:mt-16 border-t border-gray-900/10 pt-16'>
+        <div className='xl:grid xl:grid-cols-3 xl:gap-8'>
+          <div className='space-y-8'>
+            <img className="h-7" src="/favicon.ico" alt="PDF.ai logo" />
+            <div className="text-sm leading-6 text-gray-600">Chat with any PDF: ask questions, get summaries, find information, and more.</div>
+          </div>
+          <div className='mt-16 grid grid-cols-1 gap-8 xl:col-span-2 xl:mt-0'>
+            <div className="md:grid md:grid-cols-3 md:gap-8"><div><h3 className="text-sm font-semibold leading-6 text-gray-900">Products</h3><ul role="list" className="mt-6 space-y-4 list-none p-0"><li className="p-0 m-0"><a href="/use-cases" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Use cases</a></li><li className="p-0 m-0"><a href="/chrome-extension" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Chrome extension</a></li><li className="p-0 m-0"><a href="https://api.pdf.ai/" className="text-sm leading-6 text-gray-600 hover:text-gray-900">API docs</a></li><li className="p-0 m-0"><a href="https://pdf.ai/pricing" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Pricing</a></li><li className="p-0 m-0"><a href="https://pdf.ai/tutorials" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Video tutorials</a></li><li className="p-0 m-0"><a href="https://pdf.ai/resources" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Resources</a></li><li className="p-0 m-0"><a href="https://pdf.ai/blog" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a></li><li className="p-0 m-0"><a href="/faq" className="text-sm leading-6 text-gray-600 hover:text-gray-900">FAQ</a></li></ul></div><div className="mt-10 md:mt-0"><h3 className="text-sm font-semibold leading-6 text-gray-900">We also built</h3><ul role="list" className="mt-6 space-y-4 list-none p-0"><li className="p-0 m-0"><a href="https://pdf.ai/tools/resume-ai-scanner" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Resume AI Scanner</a></li><li className="p-0 m-0"><a href="https://pdf.ai/tools/invoice-ai-scanner" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Invoice AI Scanner</a></li><li className="p-0 m-0"><a href="https://pdf.ai/tools/quiz-ai-generator" className="text-sm leading-6 text-gray-600 hover:text-gray-900">AI Quiz Generator</a></li><li className="p-0 m-0"><a href="https://quickyai.com" className="text-sm leading-6 text-gray-600 hover:text-gray-900">QuickyAI</a></li><li className="p-0 m-0"><a href="https://docsium.com" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Docsium</a></li><li className="p-0 m-0"><a href="https://pdf.ai/gpts" className="text-sm leading-6 text-gray-600 hover:text-gray-900">PDF GPTs</a></li><li className="p-0 m-0"><a href="https://pdfgen.com" className="text-sm leading-6 text-gray-600 hover:text-gray-900">PDF AI generator</a></li><li className="p-0 m-0"><a href="https://pdf.ai/tools" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Other PDF tools</a></li></ul></div><div className="mt-10 md:mt-0"><h3 className="text-sm font-semibold leading-6 text-gray-900">Company</h3><ul role="list" className="mt-6 space-y-4 list-none p-0"><li className="p-0 m-0"><a href="/compare/chatpdf-alternative" className="text-sm leading-6 text-gray-600 hover:text-gray-900">PDF.ai vs ChatPDF</a></li><li className="p-0 m-0"><a href="/compare/adobe-acrobat-reader-alternative" className="text-sm leading-6 text-gray-600 hover:text-gray-900">PDF.ai vs Acrobat Reader</a></li><li className="p-0 m-0"><a href="/privacy-policy" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Legal</a></li><li className="p-0 m-0"><a href="/affiliate-program" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Affiliate program 💵</a></li><li className="p-0 m-0"><a href="/investor" className="text-sm leading-6 text-gray-600 hover:text-gray-900">Investor</a></li></ul></div></div>
+          </div>
+        </div>
+      </div>
       </footer>
     </div>
   )
